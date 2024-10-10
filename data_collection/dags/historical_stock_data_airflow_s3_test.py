f909e8cf4f7b6704e858529af60e0056279cc0ac
ikshwakalluri <alluriikshwak@gmail.com>
2024-10-10 16:00:48 -0500

    Working DAG for stock data retrieval and uploading data to AWS S3

--- data_collection/dags/historical_stock_data_airflow_s3_test.py
+++ data_collection/dags/historical_stock_data_airflow_s3_test.py
@@ -1,3 +1,4 @@
+from nis import cat
 import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -6,24 +7,28 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import json
 import pandas as pd
 import yfinance as yf
-import io
+from datetime import datetime
+
+# import io
 import os
-import concurrent.futures
+
+# import concurrent.futures
 import shutil
 
 # Define your default arguments
 default_args = {
     "owner": "airflow",
-    "start_date": airflow.utils.dates.days_ago(1),
+    "start_date": datetime.now(),
     "depends_on_past": False,
     "retries": 1,
 }
 
 # DAG DEFINITION
 with DAG(
-    dag_id="batch_stock_data_airflow_s3_test",
+    dag_id="batch_stock_data_airflow_s3_yearly",
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval="@once",
+    catchup=False,
 ) as dag:
 
     def fetch_and_store_data_locally(
@@ -42,17 +47,17 @@ with DAG(
             )
         except Exception as e:
             print(f"Error fetching data for {company_symbol}: {e}")
+            return
+
         stock_data.reset_index(inplace=True)
         stock_data["Year"] = stock_data["Date"].dt.year
-        stock_data["Month"] = stock_data["Date"].dt.month
 
         # Group data by year and save yearly data in Parquet format
-
-        for (year, month), data in stock_data.groupby(["Year", "Month"]):
+        for year, data in stock_data.groupby("Year"):
             # Create partitioned directory for company and year
             partition_dir = os.path.join(
                 base_temp_dir,
-                f"batch_{batch_num}/company={company_symbol}/year={year}/month={month}/",
+                f"batch_{batch_num}/company={company_symbol}/year={year}/",
             )
             os.makedirs(partition_dir, exist_ok=True)
 
@@ -61,55 +66,40 @@ with DAG(
                 partition_dir, f"{company_symbol}_data_{year}.parquet"
             )
             data.to_parquet(file_path, compression="snappy")
-
-    def compress_folder(folder_path):
-        shutil.make_archive(folder_path, "zip", folder_path)
-        shutil.rmtree(folder_path)
-        print(f"Compressed {folder_path} to {folder_path}.zip")
+            print(f"Saved {company_symbol} data for year {year} at {file_path}")
 
     # Function to upload batch to AWS S3
-    # def upload_batch_to_s3(base_temp_dir, batch_number, bucket_name):
-    #     s3_hook = S3Hook(aws_conn_id="aws_default")
-
-    #     # Define the specific batch directory to upload
-    #     batch_dir = os.path.join(base_temp_dir, f"batch_{batch_number}")
-
-    #     # Walk through all subdirectories and files within the batch_dir
-    #     for root, dirs, files in os.walk(batch_dir):
-    #         print(f"Processing directory: {root}")
-    #         for file in files:
-    #             local_file_path = os.path.join(root, file)
-
-    #             # Create the corresponding S3 key to maintain the directory structure
-    #             relative_path = os.path.relpath(
-    #                 local_file_path, base_temp_dir
-    #             )  # Relative path from temp_dir
-    #             s3_key = f"raw_stock_data/batch_{batch_number}/{relative_path}"
-
-    #             # Upload the file to S3
-    #             s3_hook.load_file(
-    #                 filename=local_file_path,
-    #                 key=s3_key,
-    #                 bucket_name=bucket_name,
-    #                 replace=True,
-    #             )
-    # print(f"Uploaded {file} to s3://{bucket_name}/{s3_key}")
-    def upload_compressed_to_s3(base_temp_dir, batch_num, bucket_name):
-        zip_filename = os.path.join(base_temp_dir, f"batch_{batch_num}")
+    def upload_batch_to_s3(base_temp_dir, batch_number, bucket_name):
         s3_hook = S3Hook(aws_conn_id="aws_default")
-        zip_file_path = f"{zip_filename}.zip"
-        s3_key = f"raw_stock_data/"
-        # Upload the compressed file to S3
-        s3_hook.load_file(
-            filename=zip_file_path, key=s3_key, bucket_name=bucket_name, replace=True
-        )
-        print(f"Uploaded {zip_file_path} to s3://{bucket_name}/{s3_key}")
+
+        # Define the specific batch directory to upload
+        batch_dir = os.path.join(base_temp_dir, f"batch_{batch_number}")
+
+        # Walk through all subdirectories and files within the batch_dir
+        for root, dirs, files in os.walk(batch_dir):
+            print(f"Processing directory: {root}")
+            for file in files:
+                local_file_path = os.path.join(root, file)
+
+                # Create the corresponding S3 key to maintain the directory structure
+                relative_path = os.path.relpath(
+                    local_file_path, base_temp_dir
+                )  # Relative path from temp_dir
+                s3_key = f"raw_stock_data/{relative_path}"
+
+                # Upload the file to S3
+                s3_hook.load_file(
+                    filename=local_file_path,
+                    key=s3_key,
+                    bucket_name=bucket_name,
+                    replace=True,
+                )
+        print(f"Uploaded files from batch {batch_number} to s3://{bucket_name}/")
 
     # Main function to process batches
     def process_batches():
         base_temp_dir = "/opt/airflow/data_collection/temp_data/"
         df_symbols = pd.read_csv(f"{base_temp_dir}company_tick_symbols_processed.csv")
-        df_symbols = df_symbols.head(150)
 
         bucket_name = "raw-stock-data-airflow"
         if not os.path.exists(base_temp_dir):
@@ -119,18 +109,14 @@ with DAG(
         for i in range(0, len(df_symbols), batch_size):
             batch_num = i // batch_size + 1
             batch = df_symbols.iloc[i : i + batch_size]
-            # for index, row in batch.iterrows():
-            #     print(f"Fetching and storing data for {row['Symbol']}")
-            #     company_symbol = row["Symbol"]
-            #     fetch_and_store_data_locally(company_symbol, batch_num, base_temp_dir)
+            for index, row in batch.iterrows():
+                print(f"Fetching and storing data for {row['Symbol']}")
+                fetch_and_store_data_locally(row["Symbol"], batch_num, base_temp_dir)
+
+            # Upload the batch to S3 after processing
+            upload_batch_to_s3(base_temp_dir, batch_num, bucket_name)
 
             # Clean up temp directory after processing
-            # compress_folder(f"{base_temp_dir}/batch_{batch_num}")
-            upload_compressed_to_s3(
-                base_temp_dir,
-                batch_num,
-                bucket_name,
-            )
             try:
                 shutil.rmtree(
                     f"{base_temp_dir}/batch_{batch_num}"
